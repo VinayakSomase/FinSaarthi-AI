c9ff69d827ec5d4936c22ff5fec55b42854028e5
VinayakSomase <somasevinayak28@gmail.com>
2026-07-11 19:40:35 +0530

    Use Render backend URL

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -26,10 +26,32 @@
 
 // export default api;
 
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://127.0.0.1:8000",
+// });
+
+// api.interceptors.request.use((config) => {
+//   if (typeof window !== "undefined") {
+//     const token = localStorage.getItem("token");
+
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//   }
+
+//   return config;
+// });
+
+// export default api;
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ||
+    "https://finsaarthi-api.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
